8a5d80300246be6a925e4ab47248784cfa92a255
MitsukiKz <tailorkunz@gmail.com>
2025-04-16 23:21:27 -0300

    iniciando modal

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,9 +1,10 @@
 import Section from '../Section'
-import { Item, Items, Action } from './styles'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
 import zelda from '../../assets/images/zelda.png'
 import hogwarts from '../../assets/images/fundo_hogwarts.png'
 import play from '../../assets/images/play.png'
 import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
 
 type GalleryItem = {
   type: 'image' | 'video'
@@ -24,19 +25,51 @@ const mock: GalleryItem[] = [
     url: 'https://www.youtube.com/embed/zw47_q9wbBE?si=tO6FY6SHq1CxPBF'
   }
 ]
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <Items>
-      {mock.map((media, index) => (
-        <Item key={media.url}>
-          <img src={media.url} alt={`Mídia ${index + 1} de Nome do Jogo`} />
-          <Action>
-            <img src={zoom} alt="Clique para maximizar a imagem" />
-          </Action>
-        </Item>
-      ))}
-    </Items>
-  </Section>
-)
+type Props = {
+  defaultCover: string
+  name: string
+}
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} alt="Fechar" />
+          </header>
+          <img src={hogwarts} />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
 
 export default Gallery
